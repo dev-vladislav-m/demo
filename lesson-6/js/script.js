8adaadad5a05b7a8b3c6e2d18880e362a0b2e210
dev-vladislav-m <69981582+dev-vladislav-m@users.noreply.github.com>
2021-05-04 01:19:06 +0300

    Delete lesson-6 directory

--- lesson-6/js/script.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    $('section .animate__animated').each(function() { 
-        var imagePos = $(this).offset().top;
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow+650) {
-            $(this).addClass("animate__fadeInUpBig");
-        }
-    });
-});
