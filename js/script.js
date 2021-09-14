0eee92e5c8cdc789a22ebb2a39939fe8a151900d
dev-vladislav-m <69981582+dev-vladislav-m@users.noreply.github.com>
2021-09-14 22:40:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/* Pop-Up */
+$(document).keydown(function(e) {
+    if (e.keyCode == 27) {
+        closePopUp($(".popup"))
+    }
+});
+
+function openPopUp(e) {
+    var scrollbar = parseInt(window.innerWidth) - parseInt(document.documentElement.clientWidth) + 'px';
+    $("body").css({'paddingRight' : scrollbar});
+    $("body").toggleClass("lock");
+    e.fadeIn();
+}
+
+function closePopUp(e) {
+    setTimeout(() => $("body").toggleClass("lock"), 400);
+    setTimeout(() => $("body").css({'paddingRight' : '0px'}), 400);
+    e.fadeOut();
+}
+
+$(".hero__call-me button").click(function() {
+    openPopUp($(".popup-call-me"));
+});
